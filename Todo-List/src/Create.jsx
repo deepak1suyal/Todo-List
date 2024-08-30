9cc519d7b6e5342ca30802ad6f83f252d3162bcc
deepak1suyal <dsuyal800@gmail.com>
2024-08-30 20:34:53 +0530

    Todo List

---
+++ Todo-List/src/Create.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+function Create() {
+    const [task,setTask]=useState();
+    const A=()=>{
+     axios.post('http://localhost:3001/add',{task:task})
+     .then(result=>console.log(result))
+     .catch(err=>console.log(err))
+    }
+  return (
+    <div>  
+      <input type='text' name='' id='' onChange={(e)=>setTask(e.target.value)} className='bg-black outline-none p-3 w-80 bottom-1 rounded-lg  text-white '/>
+      <button type='button' className='p-3 ml-1 bg-black text-white cursor-pointer rounded-lg' onClick={A} >Add</button>
+    </div>
+  )
+}
+
+export default Create;
+
---
+++ Todo-List/src/Home.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Create from './Create';
+import axios from 'axios';
+
+function Home() {
+
+  const [updatedtask,setupTask]=useState();  
+  const up=(id)=>{
+   axios.put('http://localhost:3001/update/'+id,{updatedtask:updatedtask})
+   .then(result=>console.log(result))
+   .catch(err=>console.log(err))
+  }
+    const [todo,setTodo]=useState([]);
+    useEffect(()=>{
+     axios.get('http://localhost:3001/get')
+     .then(result=>setTodo(result.data))
+     .catch(err=>console.log(err))
+    },[todo])
+const Edit=(id)=>{
+  axios.put('http://localhost:3001/update/'+id)
+  .then(result=>console.log(result))
+  .catch(err=>console.log(err))
+}
+const del=(id)=>{
+  axios.delete('http://localhost:3001/delete/'+id)
+  .then(result=>{
+    location.reload()
+  })
+  .catch(err=>console.log(err))
+}
+  return (
+    <div className='flex flex-col items-center '>
+<h1 className='font-medium text-3xl p-7'>Todo List</h1>
+<Create/> 
+{   todo.length===0 ? 
+<div><h1 className='font-medium text-2xl p-3'>No Record</h1></div>
+:
+    todo.map(todo=>(<div className='p-2 bg-black text-white m-2 w-96 rounded-lg flex cursor-pointer'>{todo.task}<input  onChange={(e)=>setupTask(e.target.value)} className='text-black w-40 ml-auto' type=''  />
+    <span className = ' ml-auto w-8 flex m-1 ' >
+    <img onClick={()=>up(todo._id )} className="m-2" src="https://cdn-icons-png.flaticon.com/512/12376/12376279.png" alt="" />
+    <img className=" " onClick={()=>del(todo._id)} src="https://static.vecteezy.com/system/resources/previews/000/630/479/original/vector-trash-can-icon-symbol-illustration.jpg" alt="" /></span></div>))
+} 
+    </div>
+  )
+}
+
+export default Home;
+
+
